3a85911f932aaa06945481a673f500893b2db97e
Mark Bleichert <markbleichert@gmail.com>
2015-09-09 23:54:33 +0200

    changed to es6 syntax

--- app/actions/Actions.js
+++ app/actions/Actions.js
@@ -2,21 +2,21 @@ var Constants = require('../constants/Constants');
 var Dispatcher = require('../dispatchers/Dispatcher');
 
 var Actions = {
-	addFavorite: function (item) {
+	addFavorite(item) {
 		Dispatcher.handleViewAction({
 			actionType: Constants.ADD_FAVORITE,
 			item: item
 		});
 	},
 
-	removeFavorite: function (item) {
+	removeFavorite(item) {
 		Dispatcher.handleViewAction({
 			actionType: Constants.REMOVE_FAVORITE,
 			item: item
 		});
 	},
 
-	setActiveLocation: function (name) {
+	setActiveLocation(name) {
 		Dispatcher.handleViewAction({
 			actionType: Constants.SET_ACTIVE_LOCATION,
 			name: name
