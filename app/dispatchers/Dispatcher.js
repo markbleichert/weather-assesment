29e74c4de1a596d9e94d10bbec29f21941a18b6d
Mark Bleichert <markbleichert@gmail.com>
2015-09-09 23:56:09 +0200

    changed to es6 syntax

--- app/dispatchers/Dispatcher.js
+++ app/dispatchers/Dispatcher.js
@@ -2,7 +2,7 @@ var FluxDispatcher = require('flux').Dispatcher;
 var assign = require('React/lib/Object.assign');
 
 var Dispatcher = assign(new FluxDispatcher(), {
-	handleViewAction: function (action) {
+	handleViewAction(action) {
 		this.dispatch({
 			source: 'VIEW_ACTION',
 			action: action
