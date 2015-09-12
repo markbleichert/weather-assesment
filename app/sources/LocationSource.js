e1770004e0d8c9e642cec35f63fc0f0770b42779
Mark Bleichert <markbleichert@gmail.com>
2015-09-12 22:18:19 +0200

    added some promise usages

--- app/sources/LocationSource.js
+++ app/sources/LocationSource.js
@@ -1,20 +1,31 @@
 var mockData = require('../fixtures/data-json');
+var moreData = require('../fixtures/more-json');
 
 var LocationSource = {
 	fetch() {
+		var p1 = this.loadData(mockData);
+		var p2 = this.loadData(moreData);
+
+		// merge the results of to promise calls
+		return Promise.all([p1, p2]).then((results) => {
+			return [].concat.apply([], results)
+		});
+	},
+
+	loadData(data) {
 		return new Promise((resolve, reject) => {
 			// simulate an asynchronous action where data is fetched on
 			// a remote server somewhere.
 			setTimeout(() => {
 				try {
 					// normalize data set
-					var d = this.normalize(mockData);
+					var d = this.normalize(data);
 
 					// return new normalized data
 					resolve(d);
 
 				} catch(e) {
-					reject('Something went terribly wrong');
+					reject(e);
 				}
 
 
