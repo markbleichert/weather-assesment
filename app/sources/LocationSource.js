7a84cfe0c875243c987e2442fc01f4741bdd56dc
Mark Bleichert <markbleichert@gmail.com>
2015-09-12 14:39:45 +0200

    normalize data set

--- app/sources/LocationSource.js
+++ app/sources/LocationSource.js
@@ -1,14 +1,39 @@
 var mockData = require('../fixtures/data-json');
 
 var LocationSource = {
-	fetch: function () {
-		return new Promise(function (resolve, reject) {
+	fetch() {
+		return new Promise((resolve, reject) => {
 			// simulate an asynchronous action where data is fetched on
 			// a remote server somewhere.
-			setTimeout(function () {
-				// resolve with mock data
-				resolve(mockData);
+			setTimeout(() => {
+				try {
+					// normalize data set
+					var d = this.normalize(mockData);
+
+					// return new normalized data
+					resolve(d);
+
+				} catch(e) {
+					reject('Something went terribly wrong');
+				}
+
+
 			}, 1000);
+		});
+	},
+
+	normalize(dataArray) {
+		return dataArray.map((location, index) => {
+			Object.keys(location).forEach((key) =>{
+				// format ugly 'null' strings
+				if (location[key] === 'NULL') {
+					location[key] = '--';
+				}
+			});
+			// make each entry unique
+			location['id'] = index + 1;
+
+			return location;
 		});
 	}
 };
